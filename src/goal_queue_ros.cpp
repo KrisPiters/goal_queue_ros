a194e40cd32b5e269340e332acc45214440bdcec
Kris Piters <k.piters@gmail.com>
2018-06-13 14:26:21 +0200

    Changed update markers to only change colors on new goal addition

--- src/goal_queue_ros.cpp
+++ src/goal_queue_ros.cpp
@@ -15,30 +15,25 @@ typedef actionlib::SimpleActionClient<move_base_msgs::MoveBaseAction> MoveBaseCl
 std::vector<geometry_msgs::PoseStamped> g_goals;
 std::vector<geometry_msgs::Point> g_marker_points;
 ros::Publisher g_goal_marker_pub;
+visualization_msgs::Marker g_line_strip;
 
 int g_goal_itr;
 bool g_pub_next_goal;
 
-void publish_markers(void)
+void update_markers(void)
 {
-	visualization_msgs::Marker line_strip;
-	line_strip.header.frame_id = "map";
-	line_strip.header.stamp = ros::Time::now();
-	line_strip.action = visualization_msgs::Marker::ADD;
-	line_strip.type = visualization_msgs::Marker::LINE_STRIP;
-	line_strip.scale.x = 0.1;
-	line_strip.color.r = static_cast <float> (rand()) / static_cast <float> (RAND_MAX);
-	line_strip.color.g = static_cast <float> (rand()) / static_cast <float> (RAND_MAX);
-	line_strip.color.b = static_cast <float> (rand()) / static_cast <float> (RAND_MAX);
-	line_strip.color.a = 1.0;
-	line_strip.lifetime.sec = 2.0; 
-	
-	
-	line_strip.points = g_marker_points;
-	line_strip.points.push_back(g_marker_points[0]);
-
-	g_goal_marker_pub.publish(line_strip);
-
+	g_line_strip.header.frame_id = "map";
+	g_line_strip.header.stamp = ros::Time::now();
+	g_line_strip.action = visualization_msgs::Marker::ADD;
+	g_line_strip.type = visualization_msgs::Marker::LINE_STRIP;
+	g_line_strip.scale.x = 0.1;
+	g_line_strip.color.r = static_cast <float> (rand()) / static_cast <float> (RAND_MAX);
+	g_line_strip.color.g = static_cast <float> (rand()) / static_cast <float> (RAND_MAX);
+	g_line_strip.color.b = static_cast <float> (rand()) / static_cast <float> (RAND_MAX);
+	g_line_strip.color.a = 1.0;
+	g_line_strip.lifetime.sec = 2.0; 	
+	g_line_strip.points = g_marker_points;
+	g_line_strip.points.push_back(g_marker_points[0]);	
 }
 
 void goals_cb(const geometry_msgs::PoseStamped::ConstPtr& newGoal)
@@ -51,7 +46,7 @@ void goals_cb(const geometry_msgs::PoseStamped::ConstPtr& newGoal)
 	
 	// TODO tranform pose to fixed frame (default map)
 	g_marker_points.push_back(ps.pose.position);
-	//publish_markers();
+	update_markers();
 }
 
 // Called once when the goal completes
@@ -67,9 +62,10 @@ void doneCb(const actionlib::SimpleClientGoalState& state,
 	else
 		ROS_INFO("Reaching the goal has failed.");
 
-	if(g_goal_itr++ == g_goals.size())
+	/*if(g_goal_itr++ >= g_goals.size())
 		g_goal_itr = 0;
-	
+	*/
+	g_goal_itr++;
 	g_pub_next_goal = true;
 	
 }
@@ -84,7 +80,7 @@ void activeCb()
 // Called every time feedback is received for the goal
 void feedbackCb(const move_base_msgs::MoveBaseFeedback::ConstPtr& feedback)
 {
-  ROS_INFO("Got Feedback");// of length %lu", feedback->base_position.size());
+  //ROS_INFO("Got Feedback");// of length %lu", feedback->base_position.size());
 }
 
 int main(int argc, char** argv)
@@ -123,8 +119,14 @@ int main(int argc, char** argv)
 	{
 		if (g_goals.size() > 0)
 		{
+			g_goal_marker_pub.publish(g_line_strip);
+
 			if(g_pub_next_goal)
 			{
+				
+				if(g_goal_itr >= g_goals.size())
+					g_goal_itr = 0;
+				
 				move_base_msgs::MoveBaseGoal goal;
 				goal.target_pose = g_goals[g_goal_itr];
 				goal.target_pose.header.stamp = ros::Time::now();
@@ -134,7 +136,7 @@ int main(int argc, char** argv)
 			}
 			
 			
-			/*
+			/* // Old blocking code
 			move_base_msgs::MoveBaseGoal goal;
 			
 			if(goal_itr >= g_goals.size())
@@ -157,7 +159,7 @@ int main(int argc, char** argv)
 
 			ROS_INFO("Moving to goal nr: %d / %lu", goal_itr + 1, g_goals.size());
 			*/
-			publish_markers();
+			
 		}
 		ros::spinOnce();
 		r.sleep();
